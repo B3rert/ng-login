779adec8eb790e0c318d90d2f70bc3368c71fe7f
Robert L <robertmorales181@gmail.com>
2023-03-13 11:22:45 -0600

    add forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule //Forms Module
   ],
   providers: [],
   bootstrap: [AppComponent]
